2a3159a9e69c7b8fc2984ee848fb6e3537861547
Zi <hungryzi@gmail.com>
2012-07-12 22:25:57 +0800

    starts a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goldfish::Application.config.secret_token = '125fffa0d945e09ae7b2ec10130ad061d3da1fa35e6ead5fb9d677f1c71730e05b968030f94f06679abd47b5506dc5fd0af35cd463e3d76ab64de1184da1255e'
